3da7f7c27cae4e96522c5446b985937bea05afe7
Amos Thando Mpofu <56162910+theliteprogrammer@users.noreply.github.com>
2023-04-03 21:29:54 +0200

    Update Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Fibonacci {
+    /**
+     *
+     * @param n - the number of terms in fibonacci sequence
+     * @return array[n - 1] - the last element in the sequence of fibonacci numbers
+     */
+    private static long calc_fib(int n) {
+        // create an array of size n
+        long[] array = new long[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        // go through each index and sum up the previous two numbers
+        for(int i = 2; i < array.length; i++){
+            array[i] = array[i - 2] + array[i - 1];
+        }
+        return array[n];
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(calc_fib(n));
     }
 }
